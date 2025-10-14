96376b1e2f77b66642c51b3af856b91ddbdf02a3
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2025-10-13 21:22:22 -0300

    DRYCODE Footer
    PROYECTO-HTML-ASTRO
    13-10-2025
    + Optimizacion Footer, creacion de constantes.

--- src/data/ItemNavMenu.ts
+++ src/data/ItemNavMenu.ts
@@ -1,11 +1,43 @@
     interface ItemNavMenuInterface {
-    href: string;
-    texto: string;
+    href: string,
+    texto: string,
+    link: string
 }
 
 export const ItemNavMenu: ItemNavMenuInterface[] = [
-    {href: "/", texto: "Inicio"},
-    {href: "/Servicios", texto: "Servicios"},
-    {href: "/Blog", texto: "Blog"},
-    {href: "/Contacto", texto: "Contacto"},
+    {
+        href: "/", texto: "Inicio", 
+        link: ""
+    },
+    {
+        href: "/Servicios", texto: "Servicios", 
+        link: ""
+    },
+    {
+        href: "/Blog", texto: "Blog", 
+        link: ""
+    },
+    {
+        href: "/Contacto", texto: "Contacto", 
+        link: ""
+    },
+]
+
+export const ItemRedes: ItemNavMenuInterface[] = [
+    {
+        href: "https://images.vexels.com/media/users/3/223136/isolated/preview/984f500cf9de4519b02b354346eb72e0-icono-de-facebook-redes-sociales.png", 
+        texto: "Facebook", 
+        link: "https://www.facebook.com/fender.es/?locale=es_L"
+    },
+    {
+        href: "https://cdn-icons-png.flaticon.com/512/3983/3983858.png", 
+        texto: "Instagram", 
+        link: "https://www.instagram.com/fender_chile/?hl=es"
+    },
+    {
+        href: "https://cdn-icons-png.flaticon.com/256/3536/3536445.png", 
+        texto: "Whatsapp", 
+        link: "https://web.whatsapp.com/"
+    }
+
 ]
